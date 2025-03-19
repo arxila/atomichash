c2ed14d49f5983cacb4c4e09c4c23c8f0d41c9c5
Daniel Fernández <daniel@danfenz.dev>
2025-03-19 01:45:13 +0100

    Initial code for the compact arrays at Node

--- atomichash/src/main/java/io/aquen/atomichash/Node.java
+++ atomichash/src/main/java/io/aquen/atomichash/Node.java
@@ -20,37 +20,59 @@
 package io.aquen.atomichash;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.function.Consumer;
 
-final class Node implements Serializable {
+final class Node<K,V> implements Serializable {
 
     private static final long serialVersionUID = 5892440116260222326L;
 
-    final int byteMap;
+    final int level;
+    final long bitMap;
     final Object[] values;
 
 
-    Node(final int byteMap, final Object[] values) {
+    Node(final int level, final KeyValue<K,V> keyValue) {
         super();
-        this.byteMap = byteMap;
+        this.level = level;
+        this.bitMap = bitMap;
         this.values = values;
     }
 
-
-
-
-
-    /**
-     * Equivalent to Objects.equals(), but by being called only from
-     * HashEntry we might benefit from runtime profile information on the
-     * type of o1. See java.util.AbstractMap#eq().
-     *
-     * Do not replace with Object.equals until JDK-8015417 is resolved.
-     */
-    private static boolean eq(final Object o1, final Object o2) {
-        return o1 == null ? o2 == null : o1.equals(o2);
+    Node(final int level, final int bitMap, final Object[] values) {
+        super();
+        this.level = level;
+        this.bitMap = bitMap;
+        this.values = values;
     }
+
+    /*
+     * Computes the position in the compact array by using the bitmap. This bitmap will contain 1's for
+     * every position of the possible 64 (max size of the values array) that can contain an element. The
+     * position in the array will correspond to the number of positions occupied before the index, i.e. the
+     * number of bits to the right of the bit corresponding to the index for this level.
+     *
+     * This algorithm benefits from the fact that Long.bitCount is an intrinsic candidate typically implemented
+     * as a single "population count" CPU instruction, and thus the computation will be O(1).
+     */
+    private int valuePos(final Key<K> key) {
+        final long indexMask = 1L << key.indices[this.level];
+        final int pos = Long.bitCount(this.bitMap & (indexMask - 1L));
+        return ((this.bitMap & indexMask) == 0L) ? -pos : pos; // negative if absent, positive if present
+    }
+
+    @SuppressWarnings("unchecked")
+    public KeyValue<K,V> get(final Key<K> key) {
+        final int pos = valuePos(key);
+        if (pos < 0) {
+            return null;
+        }
+        final Object value = this.values[pos];
+        if (value instanceof KeyValue) {
+            return (KeyValue<K,V>) value;
+        }
+        return ((Node<K,V>)value).get(key);
+    }
+
+
 
 
 }
