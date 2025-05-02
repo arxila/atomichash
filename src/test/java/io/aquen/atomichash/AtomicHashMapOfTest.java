f5c7be1f3e7bffcca4a9653137e4820e2482b34a
Daniel Fernández <daniel@danfenz.dev>
2025-05-03 00:26:30 +0200

    Added tests for getAll

--- src/test/java/io/aquen/atomichash/AtomicHashMapOfTest.java
+++ src/test/java/io/aquen/atomichash/AtomicHashMapOfTest.java
@@ -20,7 +20,10 @@
 package io.aquen.atomichash;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AtomicHashMapOfTest {
 
